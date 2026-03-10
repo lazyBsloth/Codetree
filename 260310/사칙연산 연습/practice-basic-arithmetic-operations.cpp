71f0e7b9c7c768fe0b62ad29aae281211cbb13cb
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-10 17:02:41 +0900

    Title: 사칙연산 연습, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260310/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 30;
+    int c = a+b;
+    int d = c/8;
+    int e = c%8;
+    cout<<d<<"\n";
+    cout<<e<<"\n";
+    return 0;
+}
