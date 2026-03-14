1cdfd8fa0afd9b7fb75f9ee3edd4fde2443632eb
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-15 02:44:34 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260315/윤년인가/is-leap-year.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin>>y;
+
+    if(y%4==0){
+        if(y%100==0 && y%400!=0)
+        {
+            cout<<"false";
+        }
+        else{
+            cout<<"true";
+        }
+    }
+    // Please write your code here.
+    return 0;
+}
