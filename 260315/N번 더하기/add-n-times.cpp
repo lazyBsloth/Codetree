3f132168ea2e6f53d5acae176e45e2a562a6ba8a
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-15 02:10:28 +0900

    Title: N번 더하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260315/N번 더하기/add-n-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, n, d;
+    cin>>a>>n;
+    d=a+n;
+    for(int i=0; i<n; i++)
+    {
+        cout<<d<<endll;
+        d+n;
+    }
+
+    return 0;
+}
