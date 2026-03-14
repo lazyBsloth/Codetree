fcc7e167c0a0976ece3959adbab991245101de2e
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-15 02:35:47 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/점수 비교/score-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b,c,d;
+    cin>>a>>b;
+    cin>>c>>d;
+
+    cout<<(a>c && b>d);
+    // Please write your code here.
+    return 0;
+}
