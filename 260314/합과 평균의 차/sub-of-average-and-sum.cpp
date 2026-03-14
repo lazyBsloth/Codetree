aa140849adf3293243d81f740c3d8ae836524470
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-14 23:20:17 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260314/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin>>a>>b>>c;
+    int d = a+b+c;
+    cout<<d<<"\n";
+    cout<<d/3<<"\n";
+    cout<<d-d/3;
+    // Please write your code here.
+    return 0;
+}
