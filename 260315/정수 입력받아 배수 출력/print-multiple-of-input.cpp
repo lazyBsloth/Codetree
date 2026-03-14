401e24dabefa3a4d5f4a7b3cd384eda1e05dfc82
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-15 02:22:03 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    for(int i=1; i<6; i++)
+    {
+        cout<<a*i<<" ";
+    }
+    // Please write your code here.
+    return 0;
+}
