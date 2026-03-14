3236189f57112d18cb664a5945748a1d9103ecd0
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-15 02:41:21 +0900

    Title: 남녀노소 구분짓기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+    if(a==0){
+        if(b>=19){
+            cout<<"MAN";
+        }
+        else{
+            cout<<"BOY";
+        }
+    }
+    else{
+        if(b>=19){
+            cout<<"WOMAN";
+        }
+        else{
+            cout<<"GIRL";
+        }
+    }
+    // Please write your code here.
+    return 0;
+}
