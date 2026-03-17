df7981e371bd376d1970444b22cd4a235c7a95a2
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-17 21:53:55 +0900

    Title: 더 긴 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260317/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin>>a>>b;
+    if(a.length()>b.length()){
+        cout<<a<<" "<<a.length();
+    }
+    else if(a.length()<b.length())
+    {
+        cout<<b<<" "<<b.length();
+    }
+    else
+    {
+        cout<<"same";
+    }
+    // Please write your code here.
+    return 0;
+}
