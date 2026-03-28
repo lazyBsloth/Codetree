b29c5cc9476b765765a055819a354c8e2994b4a7
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-28 23:45:28 +0900

    Title: 특정 원소들의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4][4], total=0;
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<=i; j++){
+            total+=arr[i][j];
+        }
+    }
+
+    cout<<total;
+    // Please write your code here.
+    return 0;
+}
