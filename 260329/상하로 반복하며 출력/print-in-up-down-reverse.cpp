1907b6f323c8a49017d3f1edbbe031eb36a2a0a7
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-29 16:15:26 +0900

    Title: 상하로 반복하며 출력, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260329/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n][n];
+    for(int i=0; i<n; i++){
+        int start=i;
+        for(int j=0; j<n; j++){
+            if(j%2==0) arr[i][j]=start+1;
+            else arr[i][j] = n-i
+            cout<<arr[i][j]
+        }
+        cout<<endl;
+    }
+
+
+    // Please write your code here.
+    return 0;
+}
