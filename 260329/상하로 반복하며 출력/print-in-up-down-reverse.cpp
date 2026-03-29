be90fea1b53df6100d2c3144633c5af3f3ad99e1
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-03-29 16:16:37 +0900

    Title: 상하로 반복하며 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/상하로 반복하며 출력/print-in-up-down-reverse.cpp
+++ 260329/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -9,11 +9,17 @@ int main() {
         int start=i;
         for(int j=0; j<n; j++){
             if(j%2==0) arr[i][j]=start+1;
-            else arr[i][j] = n-i
-            cout<<arr[i][j]
+            else arr[i][j] = n-i;
+            cout<<arr[i][j];
         }
         cout<<endl;
     }
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout<<arr[i][j];
+    //     }
+    //     cout<<endl;
+    // }
 
 
     // Please write your code here.
