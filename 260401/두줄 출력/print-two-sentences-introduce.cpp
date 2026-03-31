cafdf30690dea59839c8c07cf68bc025102ce079
lazyBsloth <49442998+lazyBsloth@users.noreply.github.com>
2026-04-01 00:53:28 +0900

    Title: 두줄 출력, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260401/두줄 출력/print-two-sentences-introduce.cpp
+++ 260401/두줄 출력/print-two-sentences-introduce.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout<<"Hello Students!";
+    cout<<"Hello students!";
     cout<<"Welcome to LeebrosCode!";
     // Please write your code here.
     return 0;
